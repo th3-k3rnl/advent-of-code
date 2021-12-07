17128fb643c7b1a1a77612f0008139c6f6ee34a0
th3-k3rnl <aaron.ilift@gmail.com>
2021-12-07 07:57:47 +0000

    Separating out most common bit logic to use in part 2.

--- 2021/day3/sol.cpp
+++ 2021/day3/sol.cpp
@@ -1,11 +1,10 @@
 #include "io.h"
 
 
-unsigned long get_power_consumption(vector<bitset<NUM_BITS>> lines) {
-  bitset<NUM_BITS> gamma_rate;
-  bitset<NUM_BITS> epsilon;
+bitset<NUM_BITS> get_most_common_bits(vector<bitset<NUM_BITS>> lines) {
   int num_lines = lines.size();
   int sum[NUM_BITS] = {};
+  bitset<NUM_BITS> bitmask = {};
 
   for (int line = 0; line < num_lines; ++line) {
     for (int bit = 0; bit < NUM_BITS; ++bit) {
@@ -14,16 +13,24 @@ unsigned long get_power_consumption(vector<bitset<NUM_BITS>> lines) {
   }
 
   for (int bit = 0; bit < NUM_BITS; ++bit) {
-    if (sum[bit] <= num_lines / 2) {
-      gamma_rate[bit] = 0;
-      epsilon[bit] = 1;
-    } else {
-      gamma_rate[bit] = 1;
-      epsilon[bit] = 0;
-    }
+    bitmask[bit] = sum[bit] <= num_lines / 2 ? 0 : 1;
   }
+  return bitmask;
+}
+
+
+unsigned long get_power_consumption(vector<bitset<NUM_BITS>> lines) {
+  bitset<NUM_BITS> gamma_rate = get_most_common_bits(lines);
+  bitset<NUM_BITS> epsilon = ~gamma_rate;
 
   return gamma_rate.to_ulong() * epsilon.to_ulong();
+}
+
+unsigned long get_life_support_rating(vector<bitset<NUM_BITS>> lines) {
+  bitset<NUM_BITS> o2_gen_rate;
+  bitset<NUM_BITS> co2_scrub_rate;
+  
+  return o2_gen_rate.to_ulong() * co2_scrub_rate.to_ulong();
 }
 
 
@@ -45,8 +52,8 @@ int main(int argc, char** argv) {
     return EXIT_FAILURE;
   }
 
-  for (auto line : lines)
-    cout << line << '\n';
+  //  for (auto line : lines)
+  //    cout << line << '\n';
 
   power_consumption = get_power_consumption(lines);
   cout << power_consumption << '\n';
